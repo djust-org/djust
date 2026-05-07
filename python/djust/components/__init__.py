48dd286c63abc314de969bd20b7cc275f4093f84
John Tipton <github@trylinux.org>
2026-05-07 11:48:24 -0400

    Bump version to 0.9.5rc4

--- python/djust/components/__init__.py
+++ python/djust/components/__init__.py
@@ -33,7 +33,7 @@ descriptors, mixins, rust handlers, gallery).
     python manage.py component_gallery
 """
 
-__version__ = "0.9.5rc3"
+__version__ = "0.9.5rc4"
 
 # ---------------------------------------------------------------------------
 # Core component classes (original djust.components)
